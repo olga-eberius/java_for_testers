117452ff54822d27116fe8958b791eaa002e3fa2
Olga Eberius <olga.eberius@gmail.com>
2025-07-25 21:33:50 +0700

    Добавление Record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,18 +1,10 @@
 package ru.stqa.geometry.figures;
 
-public class Square {
-
-    private double side;
-
-    public Square(double side) {
-        this.side = side;
-    }
-
+public record Square(double side){
 
     public static void printSquareArea(Square s) {
         String text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
         System.out.println(text);
-
     }
 
 
