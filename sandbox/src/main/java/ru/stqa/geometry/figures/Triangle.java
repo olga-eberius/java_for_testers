271a09547c0975a41263d86fec9fe04e52aa0e03
Olga Eberius <olga.eberius@gmail.com>
2025-08-05 20:36:57 +0700

    Добавлен конструктор в класс Triangle и тесты к нему

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -2,6 +2,15 @@ package ru.stqa.geometry.figures;
 
 public record Triangle(double a, double b, double c) {
 
+    public Triangle{
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("Invalid triangle - sides violate triangle inequality");
+        }
+    }
+
     public static void printTriangleArea(Triangle t) {
         var text = String.format("Площадь треугольника со сторонами %f, %f, %f = %f", t.a, t.b, t.c, t.area());
         System.out.println(text);
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -18,5 +18,61 @@ public class TriangleTests {
         double result = t.perimeter();
         Assertions.assertEquals(12.0, result);
     }
+
+    //Невозможно создать треугольник с отрицательной стороной А
+    @Test
+    void cannotCreateTriangleWithNegativeSideA() {
+        try {
+            new Triangle(-3.0,4.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+    }
+
+    //Невозможно создать треугольник с отрицательной стороной B
+    @Test
+    void cannotCreateTriangleWithNegativeSideB() {
+        try {
+            new Triangle(3.0,-4.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+    }
+
+    //Невозможно создать треугольник с отрицательной стороной С
+    @Test
+    void cannotCreateTriangleWithNegativeSideC() {
+        try {
+            new Triangle(3.0,4.0, -5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+    }
+
+    //Невозможно создать треугольник когда сумма двух сторон равна третьей
+    @Test
+    void cannotCreateTriangleWhenSumOfTwoSidesEqualsThird() {
+        try {
+            new Triangle(3, 4, 7);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // ok
+        }
+    }
+
+    //Невозможно создать треугольник когда сумма двух сторон меньше третьей
+    @Test
+    void cannotCreateTriangleWhenSumOfTwoSidesLessThanThird() {
+        try {
+            new Triangle(3, 4, 8);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // ok
+        }
+    }
+
 }
 
